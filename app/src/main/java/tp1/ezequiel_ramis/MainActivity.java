630f678cf9b3a3a3ca8da7f86ccc820c939b9be7
Android Studio <devandroid@ort>
2019-06-06 12:08:06 -0300

    first fragment

--- app/src/main/java/tp1/ezequiel_ramis/MainActivity.java
+++ app/src/main/java/tp1/ezequiel_ramis/MainActivity.java
@@ -3,13 +3,26 @@ package tp1.ezequiel_ramis;
 import android.content.Intent;
 import android.app.Activity;
 import android.os.Bundle;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.app.Fragment;
 import android.view.View;
 
 public class MainActivity extends Activity {
+
+    FragmentManager fragmentManager;
+    FragmentTransaction fragmentTransaction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fragmentManager=getFragmentManager();
+        Fragment searchTypeFr = new SearchTypeFragment();
+
+        fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.holder, searchTypeFr);
+        fragmentTransaction.commit();
     }
 }
--- app/src/main/java/tp1/ezequiel_ramis/SearchTypeFragment.java
+++ app/src/main/java/tp1/ezequiel_ramis/SearchTypeFragment.java
@@ -3,14 +3,27 @@ package tp1.ezequiel_ramis;
 
 import android.os.Bundle;
 import android.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
-public class SearchTypeFragment extends Fragment {
+public class SearchTypeFragment extends Fragment implements View.OnClickListener {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search_type, container, false);
+        View view = inflater.inflate(R.layout.fragment_search_type, container, false);
+
+        Button byName = view.findViewById(R.id.byName);
+        Button byGeo = view.findViewById(R.id.byGeo);
+
+        byName.setOnClickListener(this);
+        byGeo.setOnClickListener(this);
+
+        return view;
+    }
+
+    public void onClick(View view){
+        Log.d("Boton apretado", String.valueOf(view.getId()));
     }
 }
