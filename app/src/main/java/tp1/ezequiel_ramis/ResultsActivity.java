ee8793a8eba089b239f953cb5a2338e398ef1cce
Android Studio <devandroid@ort>
2019-09-17 08:55:48 -0300

    done

--- app/src/main/java/tp1/ezequiel_ramis/ResultsActivity.java
+++ app/src/main/java/tp1/ezequiel_ramis/ResultsActivity.java
@@ -4,8 +4,6 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.widget.TextView;
 
-import com.microsoft.projectoxford.face.contract.Face;
-
 public class ResultsActivity extends Activity {
 
     TextView attr, stats;
@@ -24,6 +22,8 @@ public class ResultsActivity extends Activity {
 
     void statistics(Bundle bundle) {
         int sadAndBaldMen = 0;
+        int neutralKids = 0;
+        int happyAndBeardedMen = 0;
         double happy = 0, sad = 0, neutral = 0, ageAvg = 0;
         String emotion;
         for (int i=0;i<bundle.getDoubleArray("Ages").length; i++){
@@ -39,16 +39,18 @@ public class ResultsActivity extends Activity {
             /*Cantidad de pelados tristes*/
             if (bundle.getDoubleArray("Bald")[i] > 0.5 && bundle.getDoubleArray("Sad")[i] > 0.5) sadAndBaldMen++;
 
+            /*Cantidad de barbudos felices*/
+            if (bundle.getDoubleArray("Beards")[i] > 0.5 && bundle.getDoubleArray("Happy")[i] > 0.5) happyAndBeardedMen++;
+
+            /*Cantidad de infantes serios*/
+            if (bundle.getDoubleArray("Neutral")[i] > 0.5 && bundle.getDoubleArray("Ages")[i] < 10) neutralKids++;
 
             /*Atributos*/
-            attr.setText(attr.getText() + "\n");
-            attr.setText(attr.getText() + "-Edad: " + bundle.getDoubleArray("Ages")[i] + "\n");
-            attr.setText(attr.getText() + "-Sexo: " + bundle.getStringArray("Genders")[i] + "\n");
-            attr.setText(attr.getText() + "-Sonrisa: " + bundle.getDoubleArray("Smiles")[i]*100 + "%\n");
-            attr.setText(attr.getText() + "-Barba: " + bundle.getDoubleArray("Beards")[i]*100 + "%\n");
-            attr.setText(attr.getText() + "\n");
-            attr.setText(attr.getText() + "-----------------------------------------------");
-            attr.setText(attr.getText() + "\n");
+            attr.setText(attr.getText() + "\n-Edad: " + bundle.getDoubleArray("Ages")[i]);
+            attr.setText(attr.getText() + "\n-Sexo: " + bundle.getStringArray("Genders")[i]);
+            attr.setText(attr.getText() + "\n-Sonrisa: " + bundle.getDoubleArray("Smiles")[i]*100 + "%");
+            attr.setText(attr.getText() + "\n-Barba: " + bundle.getDoubleArray("Beards")[i]*100 + "%");
+            attr.setText(attr.getText() + "\n\n-----------------------------------------------\n");
         }
         ageAvg = Math.round(ageAvg/bundle.getDoubleArray("Ages").length);
 
@@ -63,5 +65,7 @@ public class ResultsActivity extends Activity {
         stats.setText("-Edad promedio: " + ageAvg + "\n");
         stats.setText(stats.getText() + "-Emocion general: " + emotion + "\n");
         stats.setText(stats.getText() + "-Cantidad de pelados tristes: " + sadAndBaldMen + "\n");
+        stats.setText(stats.getText() + "-Cantidad de barbudos felices: " + happyAndBeardedMen + "\n");
+        stats.setText(stats.getText() + "-Cantidad de infantes serios: " + neutralKids + "\n");
     }
 }
