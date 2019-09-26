9fd092e4cc66b267951454848ddad82001b0d665
Android Studio <devandroid@ort>
2019-09-26 12:08:24 -0300

    asd

--- app/src/main/java/tp1/ezequiel_ramis/MainActivity.java
+++ app/src/main/java/tp1/ezequiel_ramis/MainActivity.java
@@ -1,15 +1,34 @@
 package tp1.ezequiel_ramis;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
-public class MainActivity extends AppCompatActivity {
+import org.cocos2d.opengl.CCGLSurfaceView;
+
+public class MainActivity extends Activity {
+
+    CCGLSurfaceView view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        view = new CCGLSurfaceView(this);
+        setContentView(view);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        game game = new game(view);
+        game.startGame();
     }
 }
---
+++ app/src/main/java/tp1/ezequiel_ramis/game.java
@@ -0,0 +1,55 @@
+package tp1.ezequiel_ramis;
+
+import android.support.design.widget.Snackbar;
+import android.util.Log;
+
+import org.cocos2d.layers.Layer;
+import org.cocos2d.nodes.Director;
+import org.cocos2d.nodes.Scene;
+import org.cocos2d.nodes.Sprite;
+import org.cocos2d.opengl.CCGLSurfaceView;
+import org.cocos2d.types.CCSize;
+
+public class game {
+    CCGLSurfaceView _view;
+    CCSize _size;
+    Sprite _image;
+
+    public game(CCGLSurfaceView view) {
+        Log.d("Game", "Comienza el constructor de la clase");
+        _view= view;
+    }
+
+    public void startGame() {
+        Log.d("StartGame", "Comienza el juego");
+        Director.sharedDirector().attachInView(_view);
+        _size = Director.sharedDirector().displaySize();
+        Log.d("StartGame", "Width: " + _size.getWidth() + ", Height: " + _size.getHeight());
+        Log.d("StartGame", "Declarar e instanciar escena");
+        Scene scene = MainScene();
+        Log.d("StartGame", "Director inicia escena");
+        Director.sharedDirector().runWithScene(scene);
+    }
+
+    private Scene MainScene() {
+        Log.d("MainScene", "Comienza");
+        Scene scene = Scene.node();
+        Log.d("MainScene", "Agregar capa");
+        gameLayer layer = new gameLayer();
+        scene.addChild(layer);
+        Log.d("MainScene", "Devuelvo escena");
+        return scene;
+    }
+
+    class gameLayer extends Layer {
+        public gameLayer() {
+            Log.d("GameLayer", "Comienza el constructor");
+            Log.d("GameLayer", "Agrego imagen");
+            setImage();
+        }
+
+        void setImage() {
+            Log.d("SetImage", "Comienzo a ubicar imagen");
+        }
+    }
+}
