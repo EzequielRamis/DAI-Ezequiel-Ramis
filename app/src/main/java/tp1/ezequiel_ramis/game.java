e84b2082fec29aba520336a7bee2ba4a8b68d9f8
Ezequiel Ramis <ezequielramis.hello@gmail.com>
2019-11-15 15:58:52 -0300

    done

--- app/src/main/java/tp1/ezequiel_ramis/game.java
+++ app/src/main/java/tp1/ezequiel_ramis/game.java
@@ -3,6 +3,8 @@ package tp1.ezequiel_ramis;
 import android.content.res.AssetManager;
 import android.graphics.Typeface;
 import android.icu.text.AlphabeticIndex;
+import android.icu.text.LocaleDisplayNames;
+import android.media.MediaPlayer;
 import android.support.v4.content.res.ResourcesCompat;
 import android.util.Log;
 import android.view.MotionEvent;
@@ -10,17 +12,22 @@ import android.widget.ArrayAdapter;
 
 import org.cocos2d.actions.interval.MoveBy;
 import org.cocos2d.layers.Layer;
+import org.cocos2d.menus.Menu;
+import org.cocos2d.menus.MenuItemLabel;
+import org.cocos2d.nodes.CocosNode;
 import org.cocos2d.nodes.Director;
 import org.cocos2d.nodes.Label;
 import org.cocos2d.nodes.Scene;
 import org.cocos2d.nodes.Label;
 import org.cocos2d.opengl.CCGLSurfaceView;
+import org.cocos2d.opengl.Camera;
 import org.cocos2d.types.CCColor3B;
 import org.cocos2d.types.CCPoint;
 import org.cocos2d.types.CCSize;
 import org.cocos2d.types.CCTexParams;
 import org.cocos2d.utils.CCFormatter;
 
+import java.security.acl.LastOwnerException;
 import java.util.ArrayList;
 import java.util.Locale;
 import java.util.Random;
@@ -30,46 +37,37 @@ import java.util.TimerTask;
 public class game {
     CCGLSurfaceView _view;
     CCSize _size;
-    boolean left = new Random().nextBoolean();
+    int points;
+    int maxPoints;
     String[] tilesText = {
             "JAVA",
-            "FRIO",
+            "FRÍO",
             "CALOR",
             "SUEÑO",
-            "ESTRES",
-            "PRUEBAS",
+            "ESTRÉS",
+            "PRUEBA",
             "INFLACIÓN",
             "NO COMPILA",
             "ANSIEDAD",
             "ANDROID STUDIO",
             "GUIDO",
-            "DEPRESION",
-            "ORT"
+            "DEPRE",
+            "MIGRAÑA",
+            "NULL POINTER",
+            "RESFRÍO",
+            "RECUPERAR",
+            "HUMEDAD",
+            "ANUNCIOS",
+            "TRÁNSITO",
+            "OBSTÁCULO",
+            "CHIVO",
+            "YO",
+            "JAVA DE VUELTA",
+            "TAREA",
+            "TRABAJOS"
     };
-    CCPoint VEL;
-    int indexLastTile = 0;
-
-    Label player;
-    int repetitionI = 10000;
-    int repetitionF = 4000;
-    int repetitionTick = 50;
-
-    float velI = 20;
-    float velF = 40;
-    float velTick = .24f;
-
-    float cameraI = 1;
-    float cameraF = 4;
-    float cameraTick = .01f;
-
-    boolean orientation = true;
-
-    CCPoint delta;
 
     float labelHeight = 170*.75f;
-
-    ArrayList<Label> enemies;
-    int down = 0;
 
     public game(CCGLSurfaceView view) {
         Log.d("Game", "Comienza el constructor de la clase");
@@ -82,55 +80,179 @@ public class game {
         _size = Director.sharedDirector().displaySize();
         Log.d("StartGame", "Width: " + _size.getWidth() + ", Height: " + _size.getHeight());
         Log.d("StartGame", "Declarar e instanciar escena");
-        Scene scene = MainScene();
         Log.d("StartGame", "Director inicia escena");
-        Director.sharedDirector().runWithScene(scene);
+        Director.sharedDirector().runWithScene(UI(0));
+    }
+
+    private Scene UI(int index) {
+        Log.d("UI", "Comienza");
+        Scene scene = Scene.node();
+        Log.d("UI", "Agregar capa");
+        uiLayer uiLayer = new uiLayer(index);
+        scene.addChild(uiLayer);
+        Log.d("UI", "Devuelvo escena");
+        return scene;
     }
 
     private Scene MainScene() {
         Log.d("MainScene", "Comienza");
         Scene scene = Scene.node();
-        Log.d("MainScene", "Agregar capa");
-        gameLayer layer = new gameLayer();
-        scene.addChild(layer);
+        Log.d("MainScene", "Agregar capa y sonido");
+        gameLayer gamelayer = new gameLayer();
+        scene.addChild(gamelayer);
+        gamelayer.tenPoints = MediaPlayer.create(Director.sharedDirector().getActivity(), R.raw.ten_points);
+        gamelayer.fiftyPoints = MediaPlayer.create(Director.sharedDirector().getActivity(), R.raw.fifty_points);
+        gamelayer.slap = MediaPlayer.create(Director.sharedDirector().getActivity(), R.raw.slap);
+        gamelayer.bgMusic = MediaPlayer.create(Director.sharedDirector().getActivity(), R.raw.un_problema_menos);
+        gamelayer.bgMusic.setLooping(true);
+        gamelayer.bgMusic.setVolume(.33f,.33f);
+        gamelayer.bgMusic.start();
         Log.d("MainScene", "Devuelvo escena");
         return scene;
     }
 
+    class uiLayer extends Layer {
+        int index;
+        Menu menu;
+        MenuItemLabel menuItemLabel;
+        Label bienvenido;
+        Label puntaje;
+        Label puntajeMax;
+        public uiLayer(int index) {
+            this.index = index;
+            Label button;
+            if (index == 0){
+                button = Label.label("Jugar", "", labelHeight);
+                bienvenido = Label.label("Bienvenido a esquivar problemas :D", "", labelHeight/2.5f);
+                bienvenido.setPosition(_size.getWidth()/2, _size.getHeight()/2);
+                bienvenido.setColor(new CCColor3B(255,255,255));
+                super.addChild(bienvenido);
+            }
+            else {
+                button = Label.label("Reintentar", "", labelHeight);
+                puntaje = Label.label("Puntaje: " + points, "", labelHeight/2f);
+                puntajeMax = Label.label("Puntaje máximo: " + maxPoints, "", labelHeight/2f);
+                puntaje.setColor(new CCColor3B(255,255,255));
+                puntajeMax.setColor(new CCColor3B(255,255,255));
+                puntaje.setAnchorPoint(0, 0);
+                puntajeMax.setAnchorPoint(0, 0);
+                puntaje.setPosition(_size.getWidth()/4, _size.getHeight()/1.8f);
+                puntajeMax.setPosition(_size.getWidth()/4, _size.getHeight()/2);
+                super.addChild(puntaje);
+                super.addChild(puntajeMax);
+            }
+            button.setColor(new CCColor3B(255,200,0));
+            menuItemLabel = MenuItemLabel.item(button, this, "play");
+            menu = Menu.menu(menuItemLabel);
+            menu.setPosition(_size.getWidth()/2, _size.getHeight()/4);
+            super.addChild(menu);
+        }
+
+        public void play() {
+            points = 0;
+            Director.sharedDirector().runWithScene(MainScene());
+        }
+    }
+
     class gameLayer extends Layer {
+        boolean left = new Random().nextBoolean();
+
+        CCPoint VEL;
+        int indexLastTile = 0;
+
+        Label player;
+        int repetitionI = 25000;
+        float tick = 1.0005f;
+
+        float velI = 15;
+
+        float cameraI = 3;
+
+        boolean orientation = true;
+
+        CCPoint delta;
+
+        ArrayList<Label> enemies;
+        int down = 0;
+
+        float deltaPoints;
+        float maxYlimit = 5 * labelHeight*1.5f;
+
+        float gapX = 20;
+        float gapY = 25;
+
+        MediaPlayer tenPoints;
+        MediaPlayer fiftyPoints;
+        MediaPlayer slap;
+        MediaPlayer bgMusic;
+        int lastPoint = 0;
+        Label pointsL;
         public gameLayer() {
             VEL = new CCPoint();
             VEL.x = velI;
+            VEL.y = repetitionI;
             enemies = new ArrayList<>();
             delta = new CCPoint();
             setPlayer();
-            super.schedule("setTiles", 1/30);
             super.schedule("moveCamera", 1 / 30);
+            super.schedule("setTiles", 1/30);
             super.schedule("startCollisions", 1/30);
             setIsTouchEnabled(true);
+            pointsL = Label.label("0", "", labelHeight*.75f);
+            pointsL.setColor(new CCColor3B(255,200,0));
+            pointsL.setPosition(_size.getWidth()-125, _size.getHeight()-75);
+            super.addChild(pointsL);
+            Log.d("Puntos", "Agrego puntaje");
+            super.schedule("setPoints", 1/30);
+        }
+
+        void setPoints(float time) {
+            pointsL.setString(points+"");
+            Log.d("Puntos", points+"");
+            if (points != 0 && lastPoint < points) {
+                if (points % 50 == 0) {
+                    fiftyPoints.start();
+                    //cameraI*=1.1;
+                }
+                else if (points % 10 == 0) {
+                    tenPoints.start();
+                }
+                lastPoint = points;
+            }
         }
 
         void setTiles(float time) {
-            if (indexLastTile * 100 <= _size.getHeight() + down) {
-                AddTile addTile = new AddTile((indexLastTile + 3) * labelHeight*1.25f);
+            if (indexLastTile * labelHeight <= _size.getHeight() + down) {
+                AddTile addTile = new AddTile((indexLastTile + 5) * labelHeight*1.5f);
                 addTile.run();
                 indexLastTile++;
             }
         }
 
         void moveCamera(float time) {
-            float vel = cameraI;
-            player.setPosition(player.getPositionX(), player.getPositionY()-vel);
+            player.setPosition(player.getPositionX(), player.getPositionY()-cameraI);
             for (Label e:enemies) {
-                e.setPosition(e.getPositionX(), e.getPositionY()-vel);
+                e.setPosition(e.getPositionX(), e.getPositionY() - cameraI);
             }
-            down += vel;
+            down += cameraI;
+            maxYlimit -= cameraI;
+            VEL.y /= tick + .00025f;
+            VEL.x *= tick;
+            Log.d("Children", super.getChildren().size()+"");
         }
 
         void startCollisions(float time) {
             for (Label enemy:enemies) {
-                if (isIntersected(player, enemy)) {
+                if (isIntersected(player, enemy) ||
+                    right(player) < 0 ||
+                    left(player) > _size.getWidth() ||
+                    top(player) < 0 ||
+                    bot(player) > _size.getHeight()) {
                     /*Perder*/
+                    slap.start();
+                    bgMusic.stop();
+                    if (points > maxPoints) maxPoints = points;
+                    Director.sharedDirector().runWithScene(UI(1));
                 }
             }
         }
@@ -146,27 +268,12 @@ public class game {
             public void run() {
                 int indexText = new Random().nextInt(tilesText.length);
                 Label label = Label.label(tilesText[indexText], "", labelHeight);
-                Log.d("Size", label.getHeight()+"");
-                //orientation = orientation? false:true;
-                orientation = new Random().nextBoolean();
+                orientation = orientation? false:true;
+                //orientation = new Random().nextBoolean();     Se complica demasiado
                 Timer timer = new Timer();
                 SetEnemy setEnemy = new SetEnemy(y, tilesText[indexText], orientation, VEL.x);
-                timer.schedule(setEnemy, new Random().nextInt(10000), Math.round(label.getWidth() / _size.getWidth() * repetitionI));
-                if (repetitionI > repetitionF) {
-                    repetitionI -= repetitionTick;
-                }
-                if (VEL.x < velF) {
-                    VEL.x += velTick;
-                }
-                if (cameraI < cameraF) {
-                    cameraI+= cameraTick;
-                }
-                Log.d("Avance", repetitionI + " " + VEL.x + " " + cameraI);
-            }
-
-            @Override
-            public boolean cancel() {
-                return true;
+                timer.schedule(setEnemy, new Random().nextInt(10000), Math.round(label.getWidth() / _size.getWidth() * VEL.y));
+                Log.d("Dificultad", "Repeticion: " + VEL.y + " Velocidad: " + String.format("%.1f", VEL.x));
             }
         }
 
@@ -209,14 +316,15 @@ public class game {
                 if ((velocity > 0 && enemy.getPositionX()-enemy.getWidth()/2 > _size.getWidth())
                 ||  (velocity < 0 && enemy.getPositionX()+enemy.getWidth()/2 < 0)
                 ||  (enemy.getPositionY() < - enemy.getHeight())) {
+                    removeChild(enemy, true);
+                    enemies.remove(enemy);
                     super.cancel();
                 }
             }
 
             @Override
             public boolean cancel() {
-                removeChild(enemy, true);
-                enemies.remove(enemy);
+
                 return true;
             }
         }
@@ -233,7 +341,6 @@ public class game {
             } else {
                 velocity = -velN * sigmoid(enemy.getWidth());
             }
-            //enemiesTile.add(enemy);
             super.addChild(enemy);
             enemies.add(enemy);
             Timer timer = new Timer();
@@ -244,31 +351,15 @@ public class game {
         public void setPlayer() {
             player = Label.label("YO", "", labelHeight*.75f);
             player.setColor(new CCColor3B(255, 255, 255));
-            player.setPosition(_size.getWidth() / 2, 100);
+            player.setPosition(_size.getWidth() / 2, _size.getHeight()/2);
             super.addChild(player);
         }
 
         boolean isIntersected(Label i0, Label i1) {
-            return !(left(i0) > right(i1) ||
-                    right(i0) < left(i1) ||
-                    top(i0) < bot(i1) ||
-                    bot(i0) > top(i1));
-        }
-
-        float left(Label label) {
-            return label.getPositionX() - label.getWidth() / 2;
-        }
-
-        float right(Label label) {
-            return label.getPositionX() + label.getWidth() / 2;
-        }
-
-        float top(Label label) {
-            return label.getPositionY() + label.getHeight() / 2;
-        }
-
-        float bot(Label label) {
-            return label.getPositionY() - label.getHeight() / 2;
+            return !(left(i0) + gapX > right(i1) - gapX ||
+                    right(i0) - gapX < left(i1) + gapX ||
+                    top(i0) - gapY < bot(i1) + gapY ||
+                    bot(i0) + gapY > top(i1) - gapY);
         }
 
         @Override
@@ -276,6 +367,7 @@ public class game {
             CCPoint goTo = CCPoint.ccp(event.getX(), _size.getHeight()-event.getY());
             delta.x = player.getPositionX() - goTo.x;
             delta.y = player.getPositionY() - goTo.y;
+            deltaPoints=player.getPositionY()-maxYlimit;
             return true;
         }
 
@@ -283,6 +375,10 @@ public class game {
         public boolean ccTouchesMoved(MotionEvent event) {
             CCPoint goTo = CCPoint.ccp(event.getX(), _size.getHeight()-event.getY());
             player.setPosition(goTo.x + delta.x, goTo.y + delta.y);
+            if (player.getPositionY() - maxYlimit > deltaPoints) {
+                deltaPoints=player.getPositionY()-maxYlimit;
+                if (deltaPoints/labelHeight > points) points = Math.round(deltaPoints/labelHeight);
+            }
             return true;
         }
 
@@ -298,39 +394,21 @@ public class game {
         return 2/(1 + Math.pow(Math.E, .01*w)) + .5;
     }
 
-    /*public Direction getDirection(float x1, float y1, float x2, float y2){
-        double angle = getAngle(x1, y1, x2, y2);
-        return Direction.fromAngle(angle);
-    }*/
+    float left(Label label) {
+        return label.getPositionX() - label.getWidth() / 2;
+    }
 
-    /*public double getAngle(float x1, float y1, float x2, float y2) {
+    float right(Label label) {
+        return label.getPositionX() + label.getWidth() / 2;
+    }
 
-        double rad = Math.atan2(y1-y2,x2-x1) + Math.PI;
-        return (rad*180/Math.PI + 180)%360;
-    }*/
+    float top(Label label) {
+        return label.getPositionY() + label.getHeight() / 2;
+    }
 
-    /*public enum Direction{
-        up,
-        down,
-        left,
-        right;
-        public static Direction fromAngle(double angle){
-            if(inRange(angle, 45, 135)){
-                return Direction.up;
-            }
-            else if(inRange(angle, 0,45) || inRange(angle, 315, 360)){
-                return Direction.right;
-            }
-            else if(inRange(angle, 225, 315)){
-                return Direction.down;
-            }
-            else{
-                return Direction.left;
-            }
-        }
-        private static boolean inRange(double angle, float init, float end){
-            return (angle >= init) && (angle < end);
-        }
-    }*/
+    float bot(Label label) {
+        return label.getPositionY() - label.getHeight() / 2;
+    }
+
 
 }
